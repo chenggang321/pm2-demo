6f74cf45577f59a7c208d439b33a84c5b5c9fa6f
chengganghy <chenggang@ruyucapital.com>
2019-05-10 11:37:16 +0800

    配置修改

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,34 @@
 module.exports = {
-  apps : [{
-    name: 'API',
-    script: 'app.js',
+    apps: [{
+        name: 'API',
+        script: 'app.js',
 
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }],
+        // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+        args: 'one two',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+            NODE_ENV: 'development'
+        },
+        env_production: {
+            NODE_ENV: 'production'
+        }
+    }],
 
-  deploy : {
-    production : {
-      user : 'Administrator',
-      host : '47.94.208.33',
-      ref  : 'origin/master',
-      repo : 'git@github.com:chenggang321/pm2-demo.git',
-      path : 'C:\\Users\\Administrator\\Desktop\\pm2-demo',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    deploy: {
+        production: {
+            user: 'Administrator',
+            host: '47.94.208.33',
+            ref: 'origin/master',
+            repo: 'git@github.com:chenggang321/pm2-demo.git',
+            path: 'C:\\Users\\Administrator\\Desktop\\pm2-demo',
+            ssh_options: "StrictHostKeyChecking=no",
+            env: {
+                "NODE_ENV": "production"
+            },
+            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+        }
     }
-  }
 };
